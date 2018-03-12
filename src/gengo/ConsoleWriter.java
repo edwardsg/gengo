4a6e6e88ee4c3d40dd735052631549e2b8e4c442
Gabriel Edwards <gabriel.edwards@listrak.com>
2018-03-12 15:21:34 -0400

    Console output refactor.

---
+++ src/gengo/ConsoleWriter.java
@@ -0,0 +1,68 @@
+package gengo;
+
+import gengo.grammar.GrammaticalStructure;
+import gengo.lexicon.Primes;
+import gengo.lexicon.Root;
+import gengo.phonology.Consonant;
+import gengo.phonology.Vowel;
+
+import java.util.Map;
+
+public class ConsoleWriter {
+	private final Language language;
+	
+	ConsoleWriter(Language language) {
+		this.language = language;
+	}
+	
+	public void printLanguageDetails() {
+		printConsonants();
+		printVowels();
+		printSyllableStructure();
+		printGrammar();
+		printLexicon();
+		printFooter();
+	}
+	
+	private void printConsonants() {
+		System.out.println("Consonants (" + language.phonology().consonantInventory() + "):");
+		for (Consonant consonant : language.phonology().consonants())
+			System.out.println(consonant);
+		
+		System.out.println();
+	}
+	
+	private void printVowels() {
+		System.out.println("Vowels (" + language.phonology().vowelInventory() + "):");
+		for (Vowel vowel : language.phonology().vowels())
+			System.out.println(vowel);
+		
+		System.out.println();
+	}
+	
+	private void printSyllableStructure() {
+		System.out.println("Syllable structure: " + language.phonology().syllableStructure());
+		System.out.println();
+	}
+	
+	private void printGrammar() {
+		System.out.println("Grammar:");
+		for (GrammaticalStructure structure : language.grammar().grammaticalStructures())
+			System.out.println(structure + "\n");
+		
+		System.out.println();
+	}
+	
+	private void printLexicon() {
+		System.out.println("Word roots: ");
+		for (Map.Entry<Primes.BasicPrime, Root> entry : language.lexicon().roots().entrySet())
+			System.out.println(entry.getKey().name() + ": " + entry.getValue().root());
+		
+		System.out.println();
+	}
+	
+	private void printFooter() {
+		System.out.println("Seed: " + language.seed());
+		System.out.println();
+	}
+}
--- src/gengo/LanguageGenerator.java
+++ src/gengo/LanguageGenerator.java
@@ -1,15 +1,7 @@
 package gengo;
 
-import gengo.grammar.GrammaticalStructure;
-import gengo.lexicon.Primes;
-import gengo.lexicon.Root;
-import gengo.phonology.Consonant;
-import gengo.phonology.Vowel;
-
 import java.io.IOException;
-import java.util.Map.Entry;
 
-/* Main class */
 public class LanguageGenerator {
 	public static final boolean IPA_SYLLABLE_SEPARATION = true;
 	
@@ -29,37 +21,9 @@ public class LanguageGenerator {
 		System.out.println("\nProcedural Language Generator by Gabriel Edwards\n");
 	}
 	
-	// Console output
 	private static void printToConsole() {
 		System.out.println();
-		
-		// Print consonants in language
-		System.out.println("Consonants (" + language.phonology().consonantInventory() + "):");
-		for (Consonant consonant : language.phonology().consonants()) {
-			System.out.println(consonant);
-		}
-		
-		// Print vowels in language
-		System.out.println("\nVowels (" + language.phonology().vowelInventory() + "):");
-		for (Vowel vowel : language.phonology().vowels()) {
-			System.out.println(vowel);
-		}
-		
-		// Print syllable structure
-		System.out.println("\nSyllable structure: " + language.phonology().syllableStructure());
-		
-		// Grammar
-		System.out.println("\nGrammar:");
-		for (GrammaticalStructure structure : language.grammar().grammaticalStructures())
-			System.out.println(structure + "\n");
-		
-		// Semantic roots
-		System.out.println("\nWord roots: ");
-		for (Entry<Primes.BasicPrime, Root> entry : language.lexicon().roots().entrySet())
-			System.out.println(entry.getKey().name() + ": " + entry.getValue().root());
-		
-		// Additional information
-		System.out.println("\nSeed: " + language.seed());
+		new ConsoleWriter(language).printLanguageDetails();
 	}
 	
 	private static void buildHTML() {
