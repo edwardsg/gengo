8d9f7d1d0a6b0933f459303caa34dcdcc465df96
Gabriel Edwards <gabriel.edwards@listrak.com>
2018-03-16 16:37:22 -0400

    Categories builder refactor.

--- src/gengo/phonology/PhonemeCategoriesBuilder.java
+++ src/gengo/phonology/PhonemeCategoriesBuilder.java
@@ -19,28 +19,41 @@ public class PhonemeCategoriesBuilder {
     }
 
     private void choosePhonemeTypes() {
-        // Voicing contrast
+        chooseVoicingContrasts();
+        chooseBasicPlosives();
+        chooseUvulars();
+        chooseGlottalizedConsonants();
+        chooseLaterals();
+        chooseAbsentConsonants();
+        chooseUncommonConsonants();
+        chooseNasalVowelContrast();
+    }
+
+    private void chooseVoicingContrasts() {
         int voicingContrast = RandomGen.chooseIndexByWeights(random, Data.VOICING_CONTRAST_WEIGHTS);
         if (voicingContrast == 3 || voicingContrast == 1)
             categories.plosiveVoicingContrast = true;
         if (voicingContrast == 3 || voicingContrast == 2)
             categories.fricativeVoicingContrast = true;
+    }
 
-        // Basic plosive system from /ptkbdg/
+    private void chooseBasicPlosives() {
         int plosiveSystem = RandomGen.chooseIndexByWeights(random, Data.PLOSIVE_SYSTEM_WEIGHTS);
         if (plosiveSystem == 1 || plosiveSystem == 2 || plosiveSystem == 0)
             categories.gPresent = true;
         if (plosiveSystem == 1 || plosiveSystem == 3 || plosiveSystem == 0)
             categories.pPresent = true;
+    }
 
-        // Uvular consonants
+    private void chooseUvulars() {
         int uvularConsonantType = RandomGen.chooseIndexByWeights(random, Data.UVULAR_WEIGHTS);
         if (uvularConsonantType == 1 || uvularConsonantType == 3)
             categories.uvularStopsPresent = true;
         if (uvularConsonantType == 2 || uvularConsonantType == 3)
             categories.uvularContinuantsPresent = true;
+    }
 
-        // Glottalized consonants - ejectives and implosives
+    private void chooseGlottalizedConsonants() {
         int glottalizedType = RandomGen.chooseIndexByWeights(random, Data.GLOTTALIZED_WEIGHTS);
         if (glottalizedType == 1 || glottalizedType == 4 || glottalizedType == 5 || glottalizedType == 7)
             categories.ejectivesPresent = true;
@@ -48,8 +61,9 @@ public class PhonemeCategoriesBuilder {
             categories.implosivesPresent = true;
         if (glottalizedType == 3 || glottalizedType == 5 || glottalizedType == 6 || glottalizedType == 7)
             categories.resonantsPresent = true;
+    }
 
-        // Lateral consonants - L sounds
+    private void chooseLaterals() {
         int lateralConsonantType = RandomGen.chooseIndexByWeights(random, Data.LATERAL_WEIGHTS);
         if (lateralConsonantType == 1 || lateralConsonantType == 3)
             categories.lPresent = true;
@@ -57,8 +71,9 @@ public class PhonemeCategoriesBuilder {
             categories.lateralOthersPresent = true;
         if (lateralConsonantType == 3 || lateralConsonantType == 4)
             categories.lateralObstruentsPresent = true;
+    }
 
-        // Missing consonants
+    private void chooseAbsentConsonants() {
         int absentConsonantType = RandomGen.chooseIndexByWeights(random, Data.COMMON_ABSENCE_WEIGHTS);
         if (absentConsonantType == 1 || absentConsonantType == 4)
             categories.bilabialsPresent = false;
@@ -66,8 +81,9 @@ public class PhonemeCategoriesBuilder {
             categories.fricativesPresent = false;
         if (absentConsonantType == 3 || absentConsonantType == 4 || absentConsonantType == 5)
             categories.nasalsPresent = false;
+    }
 
-        // Presence of uncommon consonants
+    private void chooseUncommonConsonants() {
         int uncommonConsonantType = RandomGen.chooseIndexByWeights(random, Data.UNCOMMON_WEIGHTS);
         if (uncommonConsonantType == 1 || uncommonConsonantType == 5)
             categories.clicksPresent = true;
@@ -77,8 +93,9 @@ public class PhonemeCategoriesBuilder {
             categories.pharyngealsPresent = true;
         if (uncommonConsonantType == 4 || uncommonConsonantType == 5 || uncommonConsonantType == 6)
             categories.thSoundsPresent = true;
+    }
 
-        // Nasal vowel contrast
+    private void chooseNasalVowelContrast() {
         if (RandomGen.chooseIndexByWeights(random, Data.NASAL_CONTRAST_WEIGHTS) == 1)
             categories.nasalVowelContrast = true;
     }
